77b329948bfd862eb5ebf6596b0ea5ffbd92a424
Alfred Asare Amoah <alfredasare101@gmail.com>
2020-02-08 07:31:19 +0000

    Styling  collection-item and custom button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import './collection-item.style.scss';
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className='collection-item'>
@@ -15,6 +16,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton inverted>Add to cart</CustomButton>
     </div>
 );
 
