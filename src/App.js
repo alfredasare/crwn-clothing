5d0e88639b1f7f1ee770d41bfab06697c3146891
Alfred Asare Amoah <alfredasare101@gmail.com>
2020-03-04 04:21:48 +0000

    updated our applicationApp component, sign-in component and sign-up component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 // noinspection ES6CheckImport
@@ -12,34 +12,26 @@ import {selectCurrentUser} from "./redux/user/user.selectors";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import {checkUserSession} from "./redux/user/user.actions";
 
-class App extends React.Component {
+const App = ({checkUserSession, currentUser}) => {
 
-    unsubscribeFromAuth = null;
-
-    componentDidMount() {
-        const { checkUserSession } = this.props;
+    useEffect(() => {
         checkUserSession();
-    }
+    }, [checkUserSession]);
 
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+                <Route exact path='/checkout' component={CheckoutPage}/>
+                <Route exact path='/signin'
+                       render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
+            </Switch>
+        </div>
+    );
+};
 
-    render() {
-        return (
-            <div>
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/shop' component={ShopPage}/>
-                    <Route exact path='/checkout' component={CheckoutPage}/>
-                    <Route exact path='/signin'
-                           render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
-                </Switch>
-            </div>
-        );
-    }
-}
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {ReactComponent as Logo} from '../../assets/crown.svg'
-import { auth } from "../../firebase/firebase.utils";
 import { connect } from 'react-redux';
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropDown from "../cart-dropdown/cart-dropdown.component";
