0642a86b3647a839282128623fbd1efb99cfb842
Alfred Amoah <alfredasare101@gmail.com>
2020-04-24 21:24:59 +0000

    added react lazy and suspense

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,15 @@
 import React from "react";
+import Spinner from "../spinner/spinner.component";
 
-import {SpinnerContainer, SpinnerOverlay} from "./with-spinner.styles";
 
 const WithSpinner = WrappedComponent => {
-    const Spinner = ({isLoading, ...otherProps}) => {
+    return ({isLoading, ...otherProps}) => {
         return isLoading ? (
-            <SpinnerOverlay>
-                <SpinnerContainer/>
-            </SpinnerOverlay>
+            <Spinner/>
         ) : (
             <WrappedComponent {...otherProps}/>
         );
     };
-    return Spinner;
 };
 
 export default WithSpinner;
