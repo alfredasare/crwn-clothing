aee19da4c1e12ff52fcc578fa99d970b59ad85d4
Alfred Amoah <alfredasare101@gmail.com>
2020-04-25 20:05:20 +0000

    optimized web app with lazy, Suspense and memo

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {selectCurrentUser} from "./redux/user/user.selectors";
 import {checkUserSession} from "./redux/user/user.actions";
 import {GlobalStyle} from "./global.styles";
 import Spinner from "./components/spinner/spinner.component";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 
 const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
 const ShopPage = lazy(() => import("./pages/shop/shop.component"));
@@ -25,14 +26,16 @@ const App = ({checkUserSession, currentUser}) => {
             <GlobalStyle/>
             <Header/>
             <Switch>
-                <Suspense fallback={<Spinner/>}>
-                    <Route exact path='/' component={HomePage}/>
+                <ErrorBoundary>
+                    <Suspense fallback={<Spinner/>}>
+                        <Route exact path='/' component={HomePage}/>
 
-                    <Route path='/shop' component={ShopPage}/>
-                    <Route exact path='/checkout' component={CheckoutPage}/>
-                    <Route exact path='/signin'
-                           render={() => currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
-                </Suspense>
+                        <Route path='/shop' component={ShopPage}/>
+                        <Route exact path='/checkout' component={CheckoutPage}/>
+                        <Route exact path='/signin'
+                               render={() => currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
+                    </Suspense>
+                </ErrorBoundary>
             </Switch>
         </div>
     );
